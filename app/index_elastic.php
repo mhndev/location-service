b11ef1f2ac5464625ae9d235f393528fba8708c0
Payam Naderi <naderi.payam@gmail.com>
2016-12-07 11:59:29 +0330

    fix index in elastic

--- app/index_elastic.php
+++ app/index_elastic.php
@@ -1,40 +1,36 @@
 <?php
 include 'vendor/autoload.php';
 
-
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
-
 
 use Elasticsearch\ClientBuilder;
 
 $client = ClientBuilder::create()->build();
 //$deleteParams = ['index' => 'digipeyk'];
 
-$repository = new \mhndev\locationService\services\ElasticSearch($client, 'digipeyk', 'places');
+$index = 'digipeyk';
+$type = 'places';
+
+$repository = new \mhndev\locationService\services\ElasticSearch($client, $index, $type);
 
 try{
-
-    exec('curl -XDELETE '.env('ELASTIC_DB_HOST').':'.env('ELASTIC_DB_PORT').'/digipeyk/location');
-    exec('curl -XDELETE '.env('ELASTIC_DB_HOST').':'.env('ELASTIC_DB_PORT').'/digipeyk/place');
-
+    exec('curl -XDELETE '.env('ELASTIC_DB_HOST').':'.env('ELASTIC_DB_PORT').'/'.$index.'/'.$type);
     exec('curl -XDELETE '.env('ELASTIC_DB_HOST').':'.env('ELASTIC_DB_PORT').'/digipeyk');
-
-
     //$repository->deleteIndex('digipeyk');
 }catch (Elasticsearch\Common\Exceptions\Missing404Exception $exception){
     // do nothing
 }
 
 exec('
-    curl -XPUT '.env('ELASTIC_DB_HOST').':'.env('ELASTIC_DB_PORT').'/digipeyk -d \'{
+    curl -XPUT '.env('ELASTIC_DB_HOST').':'.env('ELASTIC_DB_PORT').'/'.$index.' -d \'{
         "settings" : {
             "number_of_shards" : 1
         },
         "mappings" : {
-            "place" : {
+            "'.$type.'" : {
                 "properties" : {
-                    "place" : { "type" : "geo_point"}
+                    "location" : { "type" : "geo_point"}
                 }
             }
         }
@@ -44,19 +40,23 @@ exec('
 $intersections = json_decode(file_get_contents('/docker/data/feed-locations/tehran_intersection.json'), true);
 $squares = json_decode(file_get_contents('/docker/data/feed-locations/tehran_squares.json'), true);
 
-
 $locations = array_merge($intersections, $squares);
-
 $i = 1;
 
 foreach($locations as $location){
 
     echo $i."\n";
 
-    $params['index'] = 'digipeyk';
+    $location['location'] = ['lat' => $location['latitude'], 'lon' => $location['longitude'] ];
+
+    unset($location['latitude']);
+    unset($location['longitude']);
+
+    $params['index'] = $index;
     $params['id'] = $location['id'];
-    $params['type'] = 'location';
+    $params['type'] = $type;
     $params['body'] = $location;
+
 
     $response = $client->index($params);
 
