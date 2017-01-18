85f1689aa6b3904b32dbdac2e747028223a36c65
Payam Naderi <naderi.payam@gmail.com>
2017-01-18 11:27:57 +0330

    improve docker scripts

--- .docker-compose/web-server/bootup.sh
+++ .docker-compose/web-server/bootup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+printf "\033[0;32m > Bootup ...\n"
 
 rm /docker/initialized
 
-composer install --ignore-platform-reqs --no-dev
-cp /docker/app/.env ./
+## install composer
+printf "\033[0;32m > Installing Composer Packages ...\n"
+(cd /var/www/html/ && composer install --ignore-platform-reqs)
 
+printf "\033[0;32m > Sync Environment and Configuration Files ...\n"
+cp /docker/app/.env /var/www/html/
 chmod 0777 -R /data/logs/
 
 touch /docker/initialized
