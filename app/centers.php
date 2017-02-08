0c7ffb6ecd8c7cf0c212102a7526a94712e4e7d2
Majid Abdolhosseini <majid8911303@gmail.com>
2017-02-08 12:53:12 +0330

    complete locations

--- app/centers.php
+++ app/centers.php
@@ -4,25 +4,26 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$dir = '/home/majid/Projects/location-service/.docker-compose/web-server/feed/';
-$file = $dir.'locations/intersections.json';
+$dirData = '/home/majid/Projects/location-service/.docker-compose/web-server/feed/data/';
+$dirLocations = '/home/majid/Projects/location-service/.docker-compose/web-server/feed/locations/';
 
 
+foreach (glob($dirData."*.json") as $filename){
 
-$data = json_decode(file_get_contents($file), true);
 
-$result = [];
+    $data = json_decode(file_get_contents($filename), true);
 
-$id = 1;
-foreach ($data as $record){
 
-    $record['id'] = $id;
-    $result[] = $record;
+    $result = [];
 
-    $id ++;
+    foreach ($data as $record){
+        if(!in_array($record, $result)){
+            $result[] = $record;
+        }
+    }
+
+    $text = json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE );
+    $file = fopen( $dirLocations.basename($filename), 'w');
+    fwrite($file, $text);
+
 }
-
-
-$text = json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE );
-$file = fopen( $dir.'locations/intersections.json', 'w');
-fwrite($file, $text);
