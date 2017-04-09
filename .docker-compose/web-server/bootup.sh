b57b637cf4b61b7fcdf40dd5a83278915b7f75f8
Majid Abdolhosseini <majid8911303@gmail.com>
2017-04-09 13:27:49 +0430

    fix bootup command

--- .docker-compose/web-server/bootup.sh
+++ .docker-compose/web-server/bootup.sh
@@ -5,7 +5,7 @@ rm /docker/initialized
 
 ## install composer
 printf "\033[0;32m > Installing Composer Packages ...\n"
-(cd /var/www/html/ && composer install --ignore-platform-reqs)
+(cd /var/www/ && composer install --ignore-platform-reqs)
 
 printf "\033[0;32m > Sync Environment and Configuration Files ...\n"
 cp /docker/app/.env /var/www/html/
